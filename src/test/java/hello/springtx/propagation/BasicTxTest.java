e4f05b7c1dbfd12a957467513c8e075736acaeaf
sunwooKim <frod5@kakao.com>
2023-04-18 16:53:25 +0900

    스프링 db접근기술2 트랜잭션 - 트랜잭션 전파1 트랜잭션 전파 - 전파예제 실습(이중트랜잭션 시)

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -76,4 +76,30 @@ public class BasicTxTest {
         txManager.rollback(tx2);
         log.info("트랜잭션2 롤백 완료");
     }
+
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}",outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction() = {}",inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+
+        //여기서 핵심은 트랜잭션 매니저에 커밋을 호출한다고해서 항상 실제 커넥션에 물리 커밋이 발생하지는 않는다는 점이다.
+        //신규 트랜잭션인 경우에만 실제 커넥션을 사용해서 물리 커밋과 롤백을 수행한다. 신규 트랜잭션이 아니면
+        //실제 물리 커넥션을 사용하지 않는다.
+        //이렇게 트랜잭션이 내부에서 추가로 사용되면 트랜잭션 매니저에 커밋하는 것이 항상 물리 커밋으로
+        //이어지지 않는다. 그래서 이 경우 논리 트랜잭션과 물리 트랜잭션을 나누게 된다. 또는 외부 트랜잭션과 내부
+        //트랜잭션으로 나누어 설명하기도 한다.
+        //트랜잭션이 내부에서 추가로 사용되면, 트랜잭션 매니저를 통해 논리 트랜잭션을 관리하고, 모든 논리
+        //트랜잭션이 커밋되면 물리 트랜잭션이 커밋된다고 이해하면 된다.
+    }
 }
