10ba2d13d3c68cc76e986cd659e8e9da30b6c57b
sunwooKim <frod5@kakao.com>
2023-04-19 14:39:02 +0900

    스프링 db접근기술2 트랜잭션 - 트랜잭션 전파2 트랜잭션 전파 활용 -  복구 Requires_new (Repository, Service 모두 @Transactional선언)

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -172,4 +172,45 @@ class MemberServiceTest {
         //내부 트랜잭션이 롤백 되었는데, 외부 트랜잭션이 커밋되면 UnexpectedRollbackException 예외가 발생한다.
         //rollbackOnly 상황에서 커밋이 발생하면 UnexpectedRollbackException 예외가 발생한다.
     }
+
+    /**
+     * memberService    @Transactional : on
+     * memberRepository @Transactional : on
+     * logRepository    @Transactional : on(REQUIRES_NEW) exception
+     */
+    @Test
+    void recoverException_success() {
+        //given
+        String username = "로그예외_recoverException_success";
+
+        //when
+        memberService.joinV2(username);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+
+        //flow
+        //LogRepository 에서 예외가 발생한다. 예외를 던지면 LogRepository 의 트랜잭션 AOP가 해당 예외를 받는다.
+        //REQUIRES_NEW 를 사용한 신규 트랜잭션이므로 물리 트랜잭션을 롤백한다. 물리 트랜잭션을 롤백했으므로
+        //rollbackOnly 를 표시하지 않는다. 여기서 REQUIRES_NEW 를 사용한 물리 트랜잭션은 롤백되고 완전히 끝이 나버린다.
+        //이후 트랜잭션 AOP는 전달 받은 예외를 밖으로 던진다.
+        //예외가 MemberService 에 던져지고, MemberService 는 해당 예외를 복구한다. 그리고 정상적으로 리턴한다.
+        //정상 흐름이 되었으므로 MemberService 의 트랜잭션 AOP는 커밋을 호출한다.
+        //커밋을 호출할 때 신규 트랜잭션이므로 실제 물리 트랜잭션을 커밋해야 한다. 이때 rollbackOnly 를 체크한다.
+        //rollbackOnly 가 없으므로 물리 트랜잭션을 커밋한다.
+        //이후 정상 흐름이 반환된다.
+        //결과적으로 회원 데이터는 저장되고, 로그 데이터만 롤백 되는 것을 확인할 수 있다.
+
+        //정리
+        //논리 트랜잭션은 하나라도 롤백되면 관련된 물리 트랜잭션은 롤백되어 버린다.
+        //이 문제를 해결하려면 REQUIRES_NEW 를 사용해서 트랜잭션을 분리해야 한다.
+        //참고로 예제를 단순화 하기 위해 MemberService 가 MemberRepository , LogRepository 만 호출하지만 실제로는 더 많은 리포지토리들을 호출하고 그 중에 LogRepository 만 트랜잭션을
+        //분리한다고 생각해보면 이해하는데 도움이 될 것이다.
+
+        //주의
+        //REQUIRES_NEW 를 사용하면 하나의 HTTP 요청에 동시에 2개의 데이터베이스 커넥션을 사용하게 된다.
+        //따라서 성능이 중요한 곳에서는 이런 부분을 주의해서 사용해야 한다.
+        //REQUIRES_NEW 를 사용하지 않고 문제를 해결할 수 있는 단순한 방법이 있다면, 그 방법을 선택하는 것이 더 좋다.
+    }
 }
