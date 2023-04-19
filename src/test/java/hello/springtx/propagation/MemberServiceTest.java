9455e2d133e220e77541202848d0b4b8daca4666
sunwooKim <frod5@kakao.com>
2023-04-19 13:46:51 +0900

    스프링 db접근기술2 트랜잭션 - 트랜잭션 전파2 트랜잭션 전파 활용 - 커밋,롤백 실습 (@Repository에 나눠서 @Transactional처리 하였음) 무조건 트랜잭션이 분리된 구조

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,12 +1,12 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Slf4j
@@ -39,7 +39,24 @@ class MemberServiceTest {
         assertTrue(logRepository.find(username).isPresent());
     }
 
+    /**
+     * memberService    @Transactional : off
+     * memberRepository @Transactional : on
+     * logRepository    @Transactional : on exception
+     */
     @Test
-    void joinV2() {
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when
+        ;
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
     }
+
 }
